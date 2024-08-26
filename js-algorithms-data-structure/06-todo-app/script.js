b60d392f34f61abc58e8b1e88445031bc40e7795
djsurgeon <djsurgeon83@gmail.com>
2024-08-27 00:55:40 +0200

    Update exercise 6 to step 49

--- js-algorithms-data-structure/06-todo-app/script.js
+++ js-algorithms-data-structure/06-todo-app/script.js
@@ -10,23 +10,82 @@ const tasksContainer = document.getElementById('tasks-container');
 const titleInput = document.getElementById('title-input');
 const dateInput = document.getElementById('date-input');
 const descriptionInput = document.getElementById('description-input');
-
 const taskData = [];
-let currentTask = {};
 
+let currentTask;
+
+currentTask = {};
+const addOrUpdateTask = () => {
+    const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
+    const taskObj = {
+        id: `${titleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
+        title: titleInput.value,
+        date: dateInput.value,
+        description: descriptionInput.value
+    };
+    if (dataArrIndex === -1) {
+        taskData.unshift(taskObj);
+    }
+    updateTaskContainer();
+    reset();
+};
+const updateTaskContainer = () => {
+    taskdContainer.innerHTML = "";
+    taskData.forEach(
+        ({ id, title, date, description }) => {
+            tasksContainer.innerHTML += `
+          <div class="task" id="${id}">
+            <p><strong>Title:</strong> ${title}</p>
+            <p><strong>Date:</strong> ${date}</p>
+            <p><strong>Description:</strong> ${description}</p>
+            <button type="button" class="btn" onclick="editTask(this)">Edit</button>
+            <button type="button" class="btn" onclick="deleteTask(this)">Delete</button>
+          </div>
+        `
+        }
+    );
+};
+const deleteTask = (buttonEl) => {
+    const dataArrIndex = taskData.findIndex((item) => item.id === buttonEl.parentElement.id);
+    buttonEl.parentElement.remove();
+    taskData.splice(dataArrIndex, 1);
+}
+const editTask = (buttonEl) => {
+    const dataArrIndex = taskData.findIndex(
+    (item) => item.id === buttonEl.parentElement.id
+  );
+  currentTask = taskData[dataArrIndex];
+  titleInput.value = currentTask.title;
+  dateInput.value = currentTask.date;
+  descriptionInput.value = currentTask.description;  
+}
+const reset = () => {
+    titleInput.value = "";
+    dateInput.value = "";
+    descriptionInput.value = "";
+    taskForm.classlist.toggle('hidden');
+    currentTask = {};
+};
 openTaskFormBtn.addEventListener("click", () => {
     taskForm.classList.toggle('hidden');
 })
 closeTaskFormBtn.addEventListener("click", () => {
-    confirmCloseDialog.showModal();
+    const formInputsContainValues = titleInput.value || dateInput.value || descriptionInput.value;
+    if (formInputsContainValues) {
+        confirmCloseDialog.showModal();
+    }
+    else {
+        reset();
+    }
 })
 cancelBtn.addEventListener("click", () => {
     confirmCloseDialog.close();
 })
 discardBtn.addEventListener("click", () => {
     confirmCloseDialog.close();
-    taskForm.classList.toggle ('hidden');
-})/*
-taskForm.addEventListener("click", (e) => {
+    reset();
+})
+taskForm.addEventListener("submit", (e) => {
     e.preventDefault();
-})*/
+    addOrUpdateTask();
+});
